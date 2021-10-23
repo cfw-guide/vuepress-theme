0bdb1a77616da64d732bb6a50059c3f195442db8
emiyl <me@emiyl.com>
2021-10-23 13:53:50 +0100

    added support for a site-wide notice at the top of page and also started work on supporting ads

--- lib/client/components/Page.vue
+++ lib/client/components/Page.vue
@@ -4,7 +4,20 @@
 
     <div class="theme-default-content">
       <h1>{{ pageTitle }}</h1>
+      <div class="custom-container tip" v-html="discordNoticeText" />
+      
+      <hr>
+      <div style="text-align: center;">
+          <div :id=adTagOne></div>
+      </div>
+      <hr>
+      
       <Content />
+      
+      <hr>
+      <div style="text-align: center;">
+          <div :id=adTagTwo></div>
+      </div>
     </div>
 
     <PageMeta />
@@ -20,11 +33,45 @@ import PageMeta from './PageMeta.vue'
 import PageNav from './PageNav.vue'
 import {
   usePageFrontmatter,
+  useSiteLocaleData,
 } from '@vuepress/client'
 import { computed } from 'vue'
 import type { DefaultThemeHomePageFrontmatter } from '../../shared'
-
 const frontmatter = usePageFrontmatter<DefaultThemeHomePageFrontmatter>()
+import { useThemeLocaleData } from '../composables'
+
+const discordNoticeText = computed(() => {
+  const themeLocale = useThemeLocaleData()
+  var discordNoticeText = themeLocale.value.discordNoticeText
+  if (discordNoticeText === null) {
+    return null
+  }
+  
+  var md = require('markdown-it')()
+  var render = md.render(discordNoticeText)
+  
+  return render
+})
+
+const adTagOne = computed(() => {
+  const themeLocale = useThemeLocaleData()
+  var adTagOne = themeLocale.value.adTagOne
+  if (adTagOne === null) {
+    return null
+  }
+  
+  return adTagOne
+})
+
+const adTagTwo = computed(() => {
+  const themeLocale = useThemeLocaleData()
+  var adTagTwo = themeLocale.value.adTagTwo
+  if (adTagTwo === null) {
+    return null
+  }
+  
+  return adTagTwo
+})
 
 const pageTitle = computed(() => {
   if (frontmatter.value.title === null) {
